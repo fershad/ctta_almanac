f727145a1aba688ba0533cfcf3714820c0e9a33b
fishintaiwan <fershad@gmail.com>
2020-02-26 13:07:19 +0800

    reinstate other pages

--- src/utils/generators/generateCoach.js
+++ src/utils/generators/generateCoach.js
@@ -1,10 +1,24 @@
-const countMatches = require('./countMatches')
 
 function generateCoach (data) {
     return {
       ...data,
-      matchCount: countMatches(data.matches),
+      matchCount: getMatchCount(data._id, data.matches),
     }
+  }
+
+  const getMatchCount = (id, matches) => {
+    let matchCount = 0
+
+
+    matches.forEach(match => {
+      match.coaches.forEach(coach => {
+         if (coach._ref == id) {
+             matchCount += 1
+            }
+          });
+        });
+
+        return matchCount
   }
 
   module.exports = generateCoach
