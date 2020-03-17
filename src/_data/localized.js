91c9ebc4e8cb99ddae240d730cace007a783553b
fishintaiwan <fershad@gmail.com>
2020-03-17 18:00:42 +0800

    fix css & add localization data

---
+++ src/_data/localized.js
@@ -0,0 +1,12 @@
+module.exports = {
+    en: {
+        footer: {
+            'homepage': 'CTTA Homepage',
+            'facebook': 'Facebook',
+            'credit': 'Built by <a href="https://www.fershad.com/en" class="link quiet">Fershad Irani</a> for Chinese Taipei Touch Association. Data taken from CTTA records, and the <a href="https://www.internationaltouch.org/almanac/" class="link quiet">FIT Official Almanac</a> | <a href="https://github.com/fishintaiwan/ctta_almanac" class="link quiet">Repo</a>'
+        }
+    },
+    tw: {
+
+    }
+}
