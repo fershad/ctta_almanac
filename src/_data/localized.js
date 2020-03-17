43e6a69f3026524d7feacf26ec92223d3d211d57
fishintaiwan <fershad@gmail.com>
2020-03-17 18:16:54 +0800

    start adding localization

--- src/_data/localized.js
+++ src/_data/localized.js
@@ -1,5 +1,44 @@
 module.exports = {
     en: {
+        singular: {
+            player: "player",
+            coach: "coach",
+            referee: "referee",
+            support: "support team",
+            match: "match",
+            event: "event",
+            tournament: "tournament",
+            team: "team",
+            touchdown: "touchdown",
+            assist: "assist"
+        },
+        plural: {
+            player: "players",
+            coach: "coaches",
+            referee: "referees",
+            support: "support team members",
+            match: "matches",
+            event: "events",
+            tournament: "tournaments",
+            team: "teams",
+            touchdown: "touchdowns",
+            assist: "assists"
+        },
+        table: {
+            date: "date",
+            event: "event",
+            division: "division",
+            opponent: "opponent",
+            score: "score",
+            result: "result",
+            teams: "teams",
+            assistsNote: "*Assists stat may not be complete due to limited data."
+        },
+        header: {
+            persons: "Persons",
+            teams: "Teams",
+            events: "Events"
+        },
         footer: {
             'homepage': 'CTTA Homepage',
             'facebook': 'Facebook',
