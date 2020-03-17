c8eb05b4b43a59f048ad1031bacd10b87cf37973
fishintaiwan <fershad@gmail.com>
2020-03-17 14:46:53 +0800

    setup supporters page

--- src/utils/generators/generateSupporter.js
+++ src/utils/generators/generateSupporter.js
@@ -4,6 +4,7 @@ function generateSupporter (data) {
     return {
       ...data,
       eventCount: data.events.length,
+      '75x': images(data.image).width(75).height(75).auto("format").url(),
       '150x': images(data.image).width(150).height(150).auto("format").url(),
       '300x': images(data.image).width(300).height(300).auto("format").url(),
       '600x': images(data.image).width(600).height(600).auto("format").url()
